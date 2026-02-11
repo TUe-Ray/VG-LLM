d4ad1247ad921650c31bacb8909ffabbece727eb
Shao-Ruei Huang <s.huang5@student.tue.nl>
2026-02-11 18:34:20 +0100

    auto git pull

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=debug_mutinode
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --gpus-per-node=4             # 依你的叢集格式：也可能是 --gpus-per-node=1
 #SBATCH --ntasks-per-node=1       # 通常 1 個 task，裡面用 torchrun 起多 GPU processes
 #SBATCH --cpus-per-task=8
